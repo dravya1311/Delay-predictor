5fb287e40671d7146e8c6eeceeaf02b0ef13eefe
dravya1311 <yravii.1311@gmail.com>
2025-12-01 10:21:59 +0530

    Update order_delay_dashboard.py

--- order_delay_dashboard.py
+++ order_delay_dashboard.py
@@ -70,7 +70,7 @@ st.markdown("---")
 # 1) Average sales per customer based on order_region
 # ---------------------------------------------------------------
 st.subheader("1) Average Sales Per Customer by Market")
-sales_region = df.groupby("market")["sales"].mean().reset_index()
+sales_region = df.groupby("Market")["sales"].mean().reset_index()
 
 fig1 = px.bar(
     sales_region,
