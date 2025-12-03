0af23c8085a368b979a558001e777f6d0d275da6
dravya1311 <yravii.1311@gmail.com>
2025-12-03 09:11:56 +0530

    Update order_delay_dashboard.py

--- order_delay_dashboard.py
+++ order_delay_dashboard.py
@@ -323,6 +323,6 @@ st.plotly_chart(fig, use_container_width=True)
 # -------------------------------------------------------------
 st.markdown("---")
 st.markdown(
-    "Created by Ravindra Yadav "
+                                            "Created by Ravindra Yadav "
 
 )
