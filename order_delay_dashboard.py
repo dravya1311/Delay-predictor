ac7c0aaac25ab590efd9e391232ed41cf86cff30
dravya1311 <yravii.1311@gmail.com>
2025-12-04 21:26:52 +0530

    Update order_delay_dashboard.py

--- order_delay_dashboard.py
+++ order_delay_dashboard.py
@@ -123,7 +123,7 @@ st.markdown("---")
 # -------------------------------------------------------------
 # 1. Delayed Orders by Region
 # -------------------------------------------------------------
-st.subheader("Delayed Orders by Region (count)"
+st.subheader("Delayed Orders by Region (count))"
 
 reg_grp = df_view.groupby("order_region")["is_delayed"].sum().reset_index()
 
