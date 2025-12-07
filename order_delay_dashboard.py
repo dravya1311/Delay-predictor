7fae031a3ccecec373b5365a72c1da811c00773a
dravya1311 <yravii.1311@gmail.com>
2025-12-07 18:59:42 +0530

    Update order_delay_dashboard.py

--- order_delay_dashboard.py
+++ order_delay_dashboard.py
@@ -352,7 +352,7 @@ st.plotly_chart(fig_std_delay, use_container_width=True)
 
 # Average delay score per product
 prod_delay = (
-    filtered.groupby("product_name")["label"]
+    df_view.groupby("product_name")["label"]
     .mean()
     .reset_index()
     .rename(columns={"label": "avg_delay"})
