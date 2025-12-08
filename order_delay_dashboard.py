860ed334b110efd6eb80d85c56aed5397e836fcd
dravya1311 <yravii.1311@gmail.com>
2025-12-08 11:43:52 +0530

    Update order_delay_dashboard.py

--- order_delay_dashboard.py
+++ order_delay_dashboard.py
@@ -140,10 +140,6 @@ status_pct = (
 
 status_pct.columns = ["order_status", "percentage"]
 
-# Display KPI Table
-st.subheader("Order Status Breakdown (%)")
-st.dataframe(status_pct)
-
 # Pie Chart
 fig = px.pie(
     status_pct,
