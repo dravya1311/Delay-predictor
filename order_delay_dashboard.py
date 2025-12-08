fe8de8c12d86dfdb78e3544649a47526b72876a0
dravya1311 <yravii.1311@gmail.com>
2025-12-08 17:01:03 +0530

    Update order_delay_dashboard.py

--- order_delay_dashboard.py
+++ order_delay_dashboard.py
@@ -251,7 +251,7 @@ product_sales = (
 
 top_rev = product_sales.sort_values("sales_per_customer", ascending=False).head(8)
 
-    fig = px.bar(
+fig = px.bar(
         top_rev, x="product_name", y="sales_per_customer",
         title="Top 5 Products by Revenue in dollars", text="sales_per_customer"
     )
